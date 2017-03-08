6699297b775058b7a52fccd59af44b7fe29cf30a
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-03-08 13:47:30 +0000

    more detailed tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,13 @@
 require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed multiples of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed multiples of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" if passed number divisible by 3 and 5' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end 
 end
