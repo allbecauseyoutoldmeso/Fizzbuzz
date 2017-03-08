2d1bfe2454dfdf12aa5521a19c261d51ae7e7484
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-03-08 14:03:15 +0000

    with alternative method

---
+++ lib/altfizzbuzz.rb
@@ -0,0 +1,17 @@
+class Integer
+
+def fizzbuzz
+  if self%3 == 0 && self%5 == 0
+    'fizzbuzz'
+  elsif self%3 == 0
+    'fizz'
+  elsif self%5 == 0
+    'buzz'
+  else
+    self
+  end
+end
+
+end
+
+puts 5.fizzbuzz
--- spec/altfizzbuzz_spec.rb
+++ spec/altfizzbuzz_spec.rb
@@ -1,13 +1,13 @@
-require 'fizzbuzz'
+require 'altfizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed multiples of 3' do
-    expect(fizzbuzz(9)).to eq 'fizz'
+    expect(9.fizzbuzz).to eq 'fizz'
   end
   it 'returns "buzz" when passed multiples of 5' do
-    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(10.fizzbuzz).to eq 'buzz'
   end
   it 'returns "fizzbuzz" if passed number divisible by 3 and 5' do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
+    expect(15.fizzbuzz).to eq "fizzbuzz"
   end
 end
